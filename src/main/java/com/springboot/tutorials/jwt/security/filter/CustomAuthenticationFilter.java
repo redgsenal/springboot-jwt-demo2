f17999b3fc3913c21a72aca5344576468c919edf
Reggie <reggie.senal@gmail.com>
2024-09-14 17:45:22 +0800

    set jwt library

--- src/main/java/com/springboot/tutorials/jwt/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/springboot/tutorials/jwt/security/filter/CustomAuthenticationFilter.java
@@ -1,20 +1,36 @@
 package com.springboot.tutorials.jwt.security.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
 
     private final AuthenticationManager authenticationManager;
 
@@ -32,8 +48,38 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        User user = (User) authentication.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256(getSecret());
+        String accessToken = generateAccessToken(user, request, algorithm, jwtExpiration);
+        String refreshToken = generateRefreshToken(user, request, algorithm, refreshExpiration);
+        response.setHeader("access_token", accessToken);
+        response.setHeader("refresh_token", refreshToken);
+    }
 
+    private JWTCreator.Builder generateJWT(User user, HttpServletRequest request, Algorithm algorithm) {
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withIssuer(request.getRequestURL().toString());
+    }
+
+    private String generateAccessToken(User user, HttpServletRequest request, Algorithm algorithm, long expiration) {
+        return generateJWT(user, request, algorithm)
+                .withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+    }
+
+    private String generateRefreshToken(User user, HttpServletRequest request, Algorithm algorithm, long expiration) {
+        return generateJWT(user, request, algorithm)
+                .withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+                .sign(algorithm);
+    }
+
+    private byte[] getSecret() {
+        if (secretKey == null || secretKey.isBlank()) {
+            throw new RuntimeException("Invalid secret key");
+        }
+        return secretKey.getBytes();
     }
 }
